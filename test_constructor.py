c421535cd52ce6d8d79747924b87d19911e805d6
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-10-13 23:59:52 -0400

    write test cases for get_freq method

--- test_constructor.py
+++ test_constructor.py
@@ -11,18 +11,26 @@ from scrabble_letters import ScrabbleLetters
 
 def test_init_constructor_with_path():
     """
-    Testing constructor of scrabble_letters with absolute path of the file
+    Testing constructor of scrabble_letters.py with absolute path of the file
     """
-    file_path = 'C:/Users/Swath/courses/comp801/homework/h2/scrabblesample.csv'
+    file_path = 'C:/Users/Swath/courses/comp801/homework/h2/scrabble.csv'
     test_scrabble_letters = ScrabbleLetters(file_path)
     actual = test_scrabble_letters.scrabble_letters
-    expected = {'a': [9, 1], 'b': [2, 3], 'c': [2, 3]}
+    expected = {'a': [9, 1], 'b': [2, 3], 'c': [2, 3],
+                'd': [4, 2], 'e': [12, 1], 'f': [2, 4],
+                'g': [3, 2], 'h': [2, 4], 'i': [9, 1],
+                'j': [1, 8], 'k': [1, 5], 'l': [4, 1],
+                'm': [2, 3], 'n': [6, 1], 'o': [8, 1],
+                'p': [2, 3], 'q': [1, 10], 'r': [6, 1],
+                's': [4, 1], 't': [6, 1], 'u': [4, 1],
+                'v': [2, 4], 'w': [2, 4], 'x': [1, 8],
+                'y': [2, 4], 'z': [1, 10], ' ': [2, 0]}
     assert actual == expected
 
 
 def test_init_constructor_with_filename():
     """
-    Testing constructor of scrabble_letters with the file name
+    Testing constructor of scrabble_letters.py with the file name
     """
     test_scrabble_letters = ScrabbleLetters('scrabble.csv')
     actual = test_scrabble_letters.scrabble_letters
@@ -34,7 +42,7 @@ def test_init_constructor_with_filename():
                 'p': [2, 3], 'q': [1, 10], 'r': [6, 1],
                 's': [4, 1], 't': [6, 1], 'u': [4, 1],
                 'v': [2, 4], 'w': [2, 4], 'x': [1, 8],
-                'y': [2, 4], 'z': [1, 10], 'blank': [2, 0]}
+                'y': [2, 4], 'z': [1, 10], ' ': [2, 0]}
     assert actual == expected
 
 
--- test_get_freq.py
+++ test_get_freq.py
@@ -4,3 +4,31 @@ Test get_freq methods of scrabble_letters
 File: test_get_freq.py
 Developer: Swathi Danturi
 """
+
+import pytest
+from scrabble_letters import ScrabbleLetters
+
+
+def test_get_freq_for_word_without_blank():
+    """
+    Testing get_freq of scrabble_letters.py without the space in a word
+    """
+    file_path = 'C:/Users/Swath/courses/comp801/homework/h2/scrabble.csv'
+    test_scrabble_letters = ScrabbleLetters(file_path)
+    actual = test_scrabble_letters.get_freq('user')
+    expected = {'u': 4, 's': 4, 'e': 12, 'r': 6}
+    assert actual == expected
+
+
+def test_get_freq_for_word_with_blank():
+    """
+    Testing get_freq of scrabble_letters.py with space in a word
+    """
+    file_path = 'C:/Users/Swath/courses/comp801/homework/h2/scrabble.csv'
+    test_scrabble_letters = ScrabbleLetters(file_path)
+    actual = test_scrabble_letters.get_freq('h i')
+    expected = {'h': 2, ' ': 9, 'i': 2}
+    assert actual == expected
+
+
+pytest.main()
